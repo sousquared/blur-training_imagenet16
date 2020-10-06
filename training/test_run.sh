fd6e50aab68055112c92cb3eb3d4820408980046
Sou Yoshihara <sou.yoshihara.72e@gmail.com>
2020-10-06 17:19:00 +0900

    remove sh files

--- training/test_run.sh
+++
@@ -1,6 +0,0 @@
-python main.py --mode blur-step-cbt -s 1 --cbt-rate 0.9 -n blur-step-cbt_decay9e-1_init-s1_new_func_test2
-python main.py --mode blur-step-cbt -s 2 --cbt-rate 0.9 -n blur-step-cbt_decay9e-1_init-s2_new_func_test
-python main.py --mode blur-step-cbt -s 3 --cbt-rate 0.9 -n blur-step-cbt_decay9e-1_init-s3_new_func_test
-python main.py --mode blur-step-cbt -s 4 --cbt-rate 0.9 -n blur-step-cbt_decay9e-1_init-s4_new_func_test
-
-python main.py --mode blur-step -n blur-step_new_func_test
